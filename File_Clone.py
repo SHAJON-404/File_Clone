a455bfcc84fa9b3f3d6fb8173bbd3b706f2e207a
SHAJON ツ゚ <shahmakhdumshajon@gmail.com>
2024-07-06 22:40:00 +0600

    Update File_Clone.py

--- File_Clone.py
+++ File_Clone.py
@@ -2,4 +2,6 @@
 #Encrypeted By SHAJON 
 #FaceBook : https://www.facebook.com/mdshahomagdum.shajon
 #YouTube : https://youtube.com/@mrlofi404?sub_confirmation=1
-import sanjida
+import os
+os.system("git pull")
+import sanjida64
